747c5d93aa1b624727521ce92b77f50c5d67c0a5
Nicolas Urbina <nicolasumadrigal@gmail.com>
2026-01-28 15:52:49 -0400

    refactor(settings): add index exports for components

---
+++ frontend/src/components/settings/index.ts
@@ -0,0 +1,5 @@
+// Componentes del módulo de configuración
+export { default as SettingsCard } from './SettingsCard';
+export { default as SettingsNav } from './SettingsNav';
+export { default as UserStatusBadge } from './UserStatusBadge';
+export { default as RoleBadge } from './RoleBadge';
